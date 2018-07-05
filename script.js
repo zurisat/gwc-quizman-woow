57bb7bd51a442906b3788cfb371d96011754fd8f
Glitch (personality-quiz-starter-code) <none>
2018-07-05 23:39:54 +0000

    💫🕌 Checkpoint ./script.js:575865/1049 ./index.html:575865/373

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var gryffindorScore = 0;
 var slytherinScore = 0;
-var hufflePuffScore = 0;
-var ravenClawScore = 0;
+var hufflepuffScore = 0;
+var ravenclawScore = 0;
 
 var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
@@ -10,27 +10,42 @@ var q1a4 = document.getElementById("q1a4");
 
 q1a1.addEventListener("click", gryffindor);
 q1a2.addEventListener("click", slytherin);
-q1a3.addEventListener("click", scoreFirstQuestion);
-q1a4.addEventListener("click", scoreFirstQuestion);
+q1a3.addEventListener("click", hufflepuff);
+q1a4.addEventListener("click", ravenclaw);
 
 function gryffindor(event) {
     gryffindorScore += 1;
     console.log("gryffindor");
-    
+    updateResult()  
 }
 
 function slytherin(event) {
     slytherinScore += 1;
     console.log("slytherin");
+    updateResult()
   } 
-function hufflePuff(event) {
-    hufflePuffScore += 1;
-    console.log("huffle Puff");
+
+function hufflepuff(event) {
+    hufflepuffScore += 1;
+    console.log("hufflepuff");
+    updateResult()
   }
-function ravenClaw(event) {
-    ravenClawScore += 1;
-    console.log("raven Claw");
+
+function ravenclaw(event) {
+    ravenclawScore += 1;
+    console.log("ravenclaw");
+    updateResult()
   }
+
+function updateResult() {
   var result = document.getElementById("result");
-  result.innerHTML = selected;
+  if (gryffindorScore >=1) {
+    result.innerHTML = "Gryffindor!";
+  } else if(slytherinScore >=1) {
+     result.innerHTML = "Slytherin!";
+  } else if(hufflepuffScore >=1) {
+    result.innerHTML = "Hufflepuff!";
+  } else {
+   result.innerHTML = "Ravenclaw!";
+  }
 }
