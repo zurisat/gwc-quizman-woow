090ed81941140072edf409c900f91ce532973fae
Glitch (personality-quiz-starter-code) <none>
2018-07-05 23:19:17 +0000

    🐆🐡 Checkpoint ./README.md:575865/1072 ./index.html:575865/1029 ./script.js:575865/1039

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var gryffindorScore = 0;
+var slytherinScore = 0;
+var hufflePuffScore = 0;
+var ravenClawScore = 0;
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+var q1a4 = document.getElementById("q1a4");
+
+q1a1.addEventListener("click", scoreFirstQuestion);
+q1a2.addEventListener("click", scoreFirstQuestion);
+q1a3.addEventListener("click", scoreFirstQuestion);
+q1a4.addEventListener("click", scoreFirstQuestion);
+
+function scoreFirstQuestion(event) {
+  
+}
