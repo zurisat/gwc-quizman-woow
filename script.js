02f96bff9aaefba362d8d185a061684067049d8a
Glitch (personality-quiz-starter-code) <none>
2018-07-05 23:29:53 +0000

    🎍😾 Checkpoint ./script.js:575865/1467

--- script.js
+++ script.js
@@ -8,11 +8,29 @@ var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
 
-q1a1.addEventListener("click", scoreFirstQuestion);
-q1a2.addEventListener("click", scoreFirstQuestion);
+q1a1.addEventListener("click", gryffindor);
+q1a2.addEventListener("click", slytherin);
 q1a3.addEventListener("click", scoreFirstQuestion);
 q1a4.addEventListener("click", scoreFirstQuestion);
 
-function scoreFirstQuestion(event) {
-  
+function gryffindor(event) {
+    gryffindorScore += 1;
+    console.log("gryffindor");
+    
+}
+
+function slytherin(event) {
+    slytherinScore += 1;
+    console.log("slytherin");
+  } 
+function hufflePuff(event) {
+    hufflePuffScore += 1;
+    console.log("huffle Puff");
+  }
+function ravenClaw(event) {
+    ravenClawScore += 1;
+    console.log("raven Claw");
+  }
+  var result = document.getElementById("result");
+  result.innerHTML = selected;
 }
