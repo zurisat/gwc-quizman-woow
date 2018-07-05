1bb6ccbc611fde0544000e360b2456bee50040b0
Glitch (personality-quiz-starter-code) <none>
2018-07-05 23:50:40 +0000

    🍯🐙 Checkpoint ./index.html:575865/70 ./script.js:575865/1485

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var gryffindorScore = 0;
 var slytherinScore = 0;
 var hufflepuffScore = 0;
 var ravenclawScore = 0;
+var questionsAnswered = 0;
 
 var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
@@ -13,39 +14,58 @@ q1a2.addEventListener("click", slytherin);
 q1a3.addEventListener("click", hufflepuff);
 q1a4.addEventListener("click", ravenclaw);
 
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2a3");
+var q2a4 = document.getElementById("q2a4");
+
+q2a1.addEventListener("click", gryffindor);
+q2a2.addEventListener("click", slytherin);
+q2a3.addEventListener("click", hufflepuff);
+q2a4.addEventListener("click", ravenclaw);
+
 function gryffindor(event) {
     gryffindorScore += 1;
     console.log("gryffindor");
-    updateResult()  
+    questionsAnswered += 1;
+  
 }
 
 function slytherin(event) {
     slytherinScore += 1;
     console.log("slytherin");
-    updateResult()
+    questionsAnswered += 1;
   } 
 
 function hufflepuff(event) {
     hufflepuffScore += 1;
     console.log("hufflepuff");
-    updateResult()
+    questionsAnswered += 1;
   }
 
 function ravenclaw(event) {
     ravenclawScore += 1;
     console.log("ravenclaw");
-    updateResult()
+    questionsAnswered += 1;
   }
+
+if(questionsAnswered = 2) {
+   updateResult(); 
+}
 
 function updateResult() {
   var result = document.getElementById("result");
-  if (gryffindorScore >=1) {
+  if (gryffindorScore > slytherinScore & hufflepuffScore & ravenclawScore) {
     result.innerHTML = "Gryffindor!";
-  } else if(slytherinScore >=1) {
+  } else if(slytherinScore > gryffindorScore & hufflepuffScore & ravenclawScore) {
      result.innerHTML = "Slytherin!";
-  } else if(hufflepuffScore >=1) {
+  } else if(hufflepuffScore > slytherinScore & gryffindorScore & ravenclawScore) {
     result.innerHTML = "Hufflepuff!";
+  } else if(ravenclawScore > slytherinScore & gryffindorScore & hufflepuffScore) {
+    result.innerHTML = "Ravenclaw!";
+  } else if(ravenclawScore & slytherinScore & gryffindorScore & hufflepuffScore = 0) {
+    result.innerHTML = "";
   } else {
-   result.innerHTML = "Ravenclaw!";
+   result.innerHTML = "Not Sure!";
   }
 }
