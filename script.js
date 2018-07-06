0f7c7ed353709d9e46d4d558a9401ba134198130
Glitch (personality-quiz-starter-code) <none>
2018-07-06 00:01:09 +0000

    💏🍽 Checkpoint ./script.js:575865/2615 ./index.html:575865/266

--- script.js
+++ script.js
@@ -2,70 +2,54 @@ var gryffindorScore = 0;
 var slytherinScore = 0;
 var hufflepuffScore = 0;
 var ravenclawScore = 0;
-var questionsAnswered = 0;
 
 var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
 
-q1a1.addEventListener("click", gryffindor);
-q1a2.addEventListener("click", slytherin);
-q1a3.addEventListener("click", hufflepuff);
-q1a4.addEventListener("click", ravenclaw);
+q1a1.addEventListener("click", gryffindor1);
+q1a2.addEventListener("click", slytherin1);
+q1a3.addEventListener("click", hufflepuff1);
+q1a4.addEventListener("click", ravenclaw1);
 
-var q2a1 = document.getElementById("q2a1");
-var q2a2 = document.getElementById("q2a2");
-var q2a3 = document.getElementById("q2a3");
-var q2a4 = document.getElementById("q2a4");
 
-q2a1.addEventListener("click", gryffindor);
-q2a2.addEventListener("click", slytherin);
-q2a3.addEventListener("click", hufflepuff);
-q2a4.addEventListener("click", ravenclaw);
-
-function gryffindor(event) {
+function gryffindor1(event) {
     gryffindorScore += 1;
     console.log("gryffindor");
-    questionsAnswered += 1;
-  
+    updateResult();  
 }
 
-function slytherin(event) {
+function slytherin1(event) {
     slytherinScore += 1;
     console.log("slytherin");
-    questionsAnswered += 1;
-  } 
+    updateResult();  
+} 
 
-function hufflepuff(event) {
+function hufflepuff1(event) {
     hufflepuffScore += 1;
     console.log("hufflepuff");
-    questionsAnswered += 1;
+    updateResult();
   }
 
-function ravenclaw(event) {
+function ravenclaw1(event) {
     ravenclawScore += 1;
     console.log("ravenclaw");
-    questionsAnswered += 1;
+    updateResult();
   }
 
-if(questionsAnswered = 2) {
-   updateResult(); 
-}
 
 function updateResult() {
   var result = document.getElementById("result");
-  if (gryffindorScore > slytherinScore & hufflepuffScore & ravenclawScore) {
+  if (gryffindorScore == 1) {
     result.innerHTML = "Gryffindor!";
-  } else if(slytherinScore > gryffindorScore & hufflepuffScore & ravenclawScore) {
+  } else if(slytherinScore == 1) {
      result.innerHTML = "Slytherin!";
-  } else if(hufflepuffScore > slytherinScore & gryffindorScore & ravenclawScore) {
+  } else if(hufflepuffScore == 1) {
     result.innerHTML = "Hufflepuff!";
-  } else if(ravenclawScore > slytherinScore & gryffindorScore & hufflepuffScore) {
+  } else if(ravenclawScore == 1) {
     result.innerHTML = "Ravenclaw!";
-  } else if(ravenclawScore & slytherinScore & gryffindorScore & hufflepuffScore = 0) {
-    result.innerHTML = "";
   } else {
-   result.innerHTML = "Not Sure!";
+   result.innerHTML = "Hmmm.. I'm not sure";
   }
 }
