7338bc244c686e5579935b10e555486d40ca41fa
Glitch (personality-quiz-starter-code) <none>
2020-05-09 12:19:09 +0000

    🎡🖐 Checkpoint ./script.js:14032647/46

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //This is where you will define the variables you will be using in your project.
 //#TODO: Create four variables to track each possible quiz outcome
 var questionCount = 0;
-var result1Score = 0;
+var result1Count = 0;
 
 
 
